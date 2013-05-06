b0a180828ae61fc71c2681dfa3eac345ec653514
KTakata <xxamagixx@gmail.com>
2013-05-06 15:15:19 +0900

    Commit Project

---
+++ SampleProject/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  SampleProject
+//
+//  Created by 髙田 健太郎 on 13/05/06.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ SampleProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SampleProject
+//
+//  Created by 髙田 健太郎 on 13/05/06.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
